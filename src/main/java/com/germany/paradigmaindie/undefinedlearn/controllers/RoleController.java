51188dbed7e488f9a21461675ed736788a7d2a78
kevin santiago rey rodriguez <kev_rey@uni-bremen.de>
2021-07-09 23:20:34 +0200

    Controller Structur updated

--- src/main/java/com/germany/paradigmaindie/undefinedlearn/controllers/RoleController.java
+++ src/main/java/com/germany/paradigmaindie/undefinedlearn/controllers/RoleController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/video")
-public class VideoController {
+@RequestMapping("/api/v1/role")
+public class RoleController {
 }
--- src/main/java/com/germany/paradigmaindie/undefinedlearn/controllers/UsersController.java
+++ src/main/java/com/germany/paradigmaindie/undefinedlearn/controllers/UsersController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/v1/users")
 public class UsersController {
 
     private final AppUserDetailsService appUserDetailsService;
