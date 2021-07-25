80f2e3d820de9c377e8a53cfc66b0b5cc7298429
kevin santiago rey rodriguez <kev_rey@uni-bremen.de>
2021-07-25 11:56:39 +0200

    Courses initial data updated

--- src/main/java/com/germany/paradigmaindie/ParadigmaIndieServer/config/SetupDataLoader.java
+++ src/main/java/com/germany/paradigmaindie/ParadigmaIndieServer/config/SetupDataLoader.java
@@ -101,56 +101,29 @@ public class SetupDataLoader implements
     }
 
     @Transactional
-    Video creaVideo(){
-
-        //VIDEO Test
+    Video creaVideo( String name, String url, String description){
         Video video = new Video();
-        video.setName("testVideo");
-        video.setUrl("VideUrl");
+        video.setName(name);
+        video.setUrl(url);
         video.setTags("testTags");
-        video.setDescription("This is the description the video");
-
-        //VIDEO Test
-        Video video1 = new Video();
-        video1.setName("testVideo");
-        video1.setUrl("VideUrl");
-        video1.setTags("testTags");
-        video1.setDescription("This is the description the video");
-        videoRepository.save(video1);
-
-        //VIDEO Test
-        Video video2 = new Video();
-        video2.setName("testVideo");
-        video2.setUrl("VideUrl");
-        video2.setTags("testTags");
-        video2.setDescription("This is the description the video");
-        videoRepository.save(video2);
+        video.setDescription(description);
         return videoRepository.save(video);
     }
 
     @Transactional
-    Category creacategorias(){
-        //VIDEO Test
+    Category creacategorias(String name){
         Category category = new Category();
-        category.setName("Docker");
-        categoriesRepository.save(category);
-
-        Category category1 = new Category();
-        category1.setName("Git");
-        categoriesRepository.save(category1);
-
-        Category category2 = new Category();
-        category2.setName("Fundamentos");
-        return categoriesRepository.save(category2);
+        category.setName(name);
+        return categoriesRepository.save(category);
     }
 
     @Transactional
-    Course creaCourses(Video video, Category category){
+    Course creaCourses(String name, String description, Set<Video> videos, Category category){
         Course course = new Course();
-        course.setName("Course Name");
-        course.setDescription("Course Description");
-        course.setTags("course tag example");
-        course.setVideos(Stream.of(video).collect(Collectors.toSet()));
+        course.setName(name);
+        course.setDescription(description);
+        course.setTags("Git");
+        course.setVideos(videos);
         course.setCategorias(Stream.of(category).collect(Collectors.toSet()));
         return courseRepository.save(course);
     }
@@ -167,11 +140,44 @@ public class SetupDataLoader implements
         createRoleIfNotFound("ROLE_ADMIN", Arrays.asList(readPrivilege, writePrivilege));
         createRoleIfNotFound("ROLE_USER", Arrays.asList(readPrivilege));
 
-        Video video = creaVideo();
-        Category category = creacategorias();
-        Course courseCreated = creaCourses(video,category);
+        Video video = creaVideo("Markdown", "https://www.youtube.com/watch?v=fxJk-AN7_-4","Markdown description");
+        Video video1 = creaVideo("Cómo instalar Git en Mac y Linux", "https://www.youtube.com/watch?v=VY7foMQLsFQ&t=485s","Cómo instalar Git en Mac y Linux | Curso de Git y GitHub Desde Cero 2021");
+        Video video2 = creaVideo("Cómo instalar Git en Windows", "https://www.youtube.com/watch?v=HZ4IjC3H2xw","Curso de Git y GitHub Desde Cero 2021");
+        Video video3 = creaVideo("Cómo configurar Git ", "https://www.youtube.com/watch?v=ZOsAfmnBq60","Cómo configurar Git | Curso de Git y GitHub Desde Cero 2021");
 
-        createUser(roleRepository.findByName("ROLE_ADMIN"),courseCreated);
+        Category category = creacategorias("GIT");
+
+
+        Course course1 = creaCourses("Git 2021", "Curso de git description esto es algo de lo que aprendera", Stream.of(video, video1, video2, video3).collect(Collectors.toSet()), category);
+
+        createUser(roleRepository.findByName("ROLE_ADMIN"),course1);
+
+        //Creacion curso 2
+
+        Video videoM = creaVideo("Wordpress | Cap 1", "https://www.youtube.com/watch?v=K42aK_OkRyc&list=PLnrCmlT1pLaA5y4QmtHDjCLav6Dx3Z8v8","Desarrollando un portafolio en Wordpress | Cap 1");
+        Video videoM1 = creaVideo("Wordpress | Cap 2", "https://www.youtube.com/watch?v=i98Ai7MGJ50&list=PLnrCmlT1pLaA5y4QmtHDjCLav6Dx3Z8v8&index=2","Desarrollando un portafolio en Wordpress | Cap 2");
+
+        Category category1 = creacategorias("Wordpress");
+
+        Course course2 = creaCourses("Wordpress 2020", "Curso de wordpress", Stream.of(videoM, videoM1).collect(Collectors.toSet()), category1);
+
+        //Creation curso 3
+
+        Video videoM01 = creaVideo("Actualizar pagina de carrito automáticamente", "https://www.youtube.com/watch?v=q10Pv7_nrjE&list=PLnrCmlT1pLaB_XloJLUwWEyUcjQEj7UR3","Desarrollando un portafolio en Wordpress | Cap 1");
+        Video videoM11 = creaVideo("Actualizar icono del carrito con Ajax - Woocommerce", "https://www.youtube.com/watch?v=EndHG8rZn1g&list=PLnrCmlT1pLaB_XloJLUwWEyUcjQEj7UR3&index=2","Desarrollando un portafolio en Wordpress | Cap 2");
+
+        Course course3 = creaCourses("WORDPRESS PARA DESARROLLADORES", "Curso de wordpress", Stream.of(videoM01, videoM11).collect(Collectors.toSet()), category1);
+
+        //Creation curso 3
+
+        Video videoP01 = creaVideo("Seguridad - Live de la comunidad Paradigma Indie", "https://www.youtube.com/watch?v=8UkHSV6Ydwg&t=4s","Desarrollando un portafolio en Wordpress | Cap 1");
+        Video videoP11 = creaVideo("Influencers en la actualidad - Live de la comunidad Paradigma Indie", "https://www.youtube.com/watch?v=5PuVafHMCFk","Desarrollando un portafolio en Wordpress | Cap 2");
+
+        Category category2 = creacategorias("Live");
+
+        Course course4 = creaCourses("ParadigmaIndie Live", "Live", Stream.of(videoM01, videoM11).collect(Collectors.toSet()), category2);
+
+
 
         alreadySetup = true;
     }
