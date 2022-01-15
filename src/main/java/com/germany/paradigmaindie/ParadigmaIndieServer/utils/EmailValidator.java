250106dd9cc3bee3b03b84eeda92b0d58f99dc20
sagivlugasi <sagivapps@gmail.com>
2022-01-15 19:50:19 +0200

    Added email regex validator

--- src/main/java/com/germany/paradigmaindie/ParadigmaIndieServer/utils/EmailValidator.java
+++ src/main/java/com/germany/paradigmaindie/ParadigmaIndieServer/utils/EmailValidator.java
@@ -3,13 +3,17 @@ package com.germany.paradigmaindie.ParadigmaIndieServer.utils;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class EmailValidator implements Predicate<String> {
+    private static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
     @Override
-    public boolean test(String s) {
-        //TODO chekc Email Regex :D
-        return true;
+    public boolean test(String emailString) {
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailString);
+        return matcher.find();
     }
 }
